36443d724b2ab0cc7b5357ba01cdd528fe7473d6
Joe Gentile <jogentile@transperfect.com>
2019-04-30 09:23:20 -0400

    Initial commit of non-versioned files.

---
+++ mp3Player/clsFileManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mp3Player
+{
+    public class clsFileManager
+    {
+        public string SourceDicretory { get; set; }
+        public List<string> MediaFiles { get; set; }
+
+        public clsFileManager()
+        {
+            MediaFiles = new List<string>();
+        }
+
+
+
+    }
+}
